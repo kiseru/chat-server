11ec695838b197ef48b5a417e637bce814f4076b
alex97 <kis.alex97@yandex.ru>
2017-05-02 21:05:56 +0300

    Incupsulate Server from Class which runs the server

--- src/main/java/com/alex/chatserver/AppInitializer.java
+++ src/main/java/com/alex/chatserver/AppInitializer.java
@@ -1,12 +1,11 @@
 package com.alex.chatserver;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class AppInitializer {
 
-    public static void main(String[] args) throws SQLException {
-
-        // Printing for test
-        System.out.println("Chat Server is up!");
+    public static void main(String[] args) throws InterruptedException, SQLException, IOException {
+        ChatServer.run();
     }
 }
