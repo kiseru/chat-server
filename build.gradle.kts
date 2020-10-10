4174ac1d12d56872edd99c79edae4352dce6a4a3
Alexandr Kiselev <kis.alex97@gmail.com>
2020-10-11 01:26:00 +0300

    Добавлены тесты для Message

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,8 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.30")
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 java {
@@ -18,4 +20,10 @@ java {
 
 application {
     mainClass.set("com.alex.chat.server.AppInitializer")
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
 }
