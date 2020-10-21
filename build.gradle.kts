ca1fe5243ce97064b7dec097877b87d4b259c6d1
Alexandr Kiselev <kis.alex97@gmail.com>
2020-10-21 23:37:50 +0300

    change gradle to gradle kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    java
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    implementation("org.slf4j:slf4j-simple:1.7.30")
+    compileOnly("org.projectlombok:lombok:1.18.14")
+    annotationProcessor("org.projectlombok:lombok:1.18.14")
+
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testCompileOnly("org.projectlombok:lombok:1.18.14")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.14")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+application {
+    mainClass.set("com.alex.chat.server.AppInitializer")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "1.8"
+}
