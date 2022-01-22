c5e97371c228a6e09196bb16c4ea6264ae08ad2f
Alexandr Kiselev <kis.alex97@gmail.com>
2022-01-23 02:30:18 +0300

    updated spring-context to version 5.3.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation("org.slf4j:slf4j-simple:1.7.30")
-    implementation("org.springframework:spring-context:5.3.14")
+    implementation("org.springframework:spring-context:5.3.15")
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
