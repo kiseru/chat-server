0f83426d16c010aec30170061a91a0231f4af9f2
Alexandr Kiselev <kis.alex97@gmail.com>
2022-07-12 11:30:19 +0300

    set kotlin version upto 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
     application
 }
 
