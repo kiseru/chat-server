0263c333e57ceb94e722442ca0cd9e1f5525202a
Alexandr Kiselev <kis.alex97@gmail.com>
2022-07-12 11:55:21 +0300

    removed AppConfig.kt

--- src/main/kotlin/com/alex/chat/server/config/AppConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.alex.chat.server.config
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@ComponentScan("com.alex.chat.server")
-@Configuration
-class AppConfig {
-}
