51c187b22154c4787ea27d28db748f2ffd760790
Alexandr Kiselev <kis.alex97@gmail.com>
2024-04-11 17:53:11 +0300

    removed channel from message transferring

--- src/main/kotlin/com/alex/chat/server/service/impl/GroupServiceImpl.kt
+++ src/main/kotlin/com/alex/chat/server/service/impl/GroupServiceImpl.kt
@@ -4,19 +4,23 @@ import com.alex.chat.server.model.Group
 import com.alex.chat.server.model.User
 import com.alex.chat.server.service.GroupService
 import com.alex.chat.server.service.UserService
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class GroupServiceImpl @Autowired constructor(
     private val userService: UserService,
 ) : GroupService {
 
-    private val groups = ConcurrentHashMap<String, Group>()
+    private val mutex = Mutex()
+    private val groups = mutableMapOf<String, Group>()
 
     override suspend fun addUserToGroup(userName: String, groupName: String): User {
-        val group = groups.computeIfAbsent(groupName) { Group(it) }
+        val group = mutex.withLock {
+            groups.getOrPut(groupName) { Group(groupName) }
+        }
         val user = userService.create(userName, group)
         group.addUser(user)
         return user
