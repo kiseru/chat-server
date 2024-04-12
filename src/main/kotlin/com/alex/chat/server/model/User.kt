c983a15c05853b7916f4a1f3c4aea3c4617962fb
Alexandr Kiselev <kis.alex97@gmail.com>
2024-04-12 12:57:22 +0300

    added stopping coroutines

--- src/main/kotlin/com/alex/chat/server/model/User.kt
+++ src/main/kotlin/com/alex/chat/server/model/User.kt
@@ -3,17 +3,28 @@ package com.alex.chat.server.model
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.takeWhile
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class User(
     val name: String,
 ) {
 
+    private val mutex = Mutex()
     private val messages = MutableSharedFlow<Message>(1, 1, BufferOverflow.DROP_OLDEST)
+
+    private var isMessaging = true
 
     suspend fun sendMessage(message: Message) {
         messages.emit(message)
     }
 
     fun messages(): Flow<Message> =
-        messages
+        messages.takeWhile { isMessaging }
+
+    suspend fun stopMessaging() {
+        mutex.withLock { isMessaging = false }
+        messages.emit(Message("stopped", name))
+    }
 }
